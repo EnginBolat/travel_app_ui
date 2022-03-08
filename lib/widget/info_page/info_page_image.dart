dd0db75cd5113389c7d29677b75d30719b523f48
Engin Bolat <engi.bolat@gmail.com>
2022-03-08 10:28:21 +0300

    Some Fix

--- lib/widget/info_page/info_page_image.dart
+++ lib/widget/info_page/info_page_image.dart
@@ -28,76 +28,73 @@ class _InfoPageImageState extends State<InfoPageImage> {
         ),
         borderRadius: BorderRadius.circular(30),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 20,
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              hotels[widget.indexId].name,
+              style: Theme.of(context).textTheme.headline4,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  hotels[widget.indexId].name,
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  "📌 " + hotels[widget.indexId].location,
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20, right: 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                  height: 30,
-                  width: 30,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      FontAwesomeIcons.share,
-                      color: Colors.white,
+                Row(
+                  children: [
+                    Text(
+                      "📌 " + hotels[widget.indexId].location,
+                      style: Theme.of(context).textTheme.headline3,
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(
-                  width: 10,
-                ),
-                SizedBox(
-                  height: 30,
-                  width: 30,
-                  child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          if (isLiked == false) {
-                            isLiked = true;
-                          } else if (isLiked == true) {
-                            (isLiked = false);
-                          }
-                        });
-                      },
-                      icon: isLiked == false
-                          ? const Icon(
-                              FontAwesomeIcons.solidHeart,
-                              color: Colors.white,
-                            )
-                          : const Icon(
-                              FontAwesomeIcons.solidHeart,
-                              color: Colors.red,
-                            )),
-                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.share,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (isLiked == false) {
+                                isLiked = true;
+                              } else if (isLiked == true) {
+                                (isLiked = false);
+                              }
+                            });
+                          },
+                          icon: isLiked == false
+                              ? const Icon(
+                                  Icons.favorite_border_outlined,
+                                  color: Colors.white,
+                                  size:26,
+                                )
+                              : const Icon(
+                                  Icons.favorite_rounded,
+                                  color: Colors.red,
+                                )),
+                    ),
+                  ],
+                )
               ],
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
